95f8f59436986d6184e50d0ce65a08be68b2cb8f
Vadim Davydov <a.e.cartoons@gmail.com>
2025-04-17 15:10:44 -0400

    feat(video-api): enable CORS in API server

--- packages/video-api/index.js
+++ packages/video-api/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import { spawn } from 'child_process';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.post('/generate-video', async (req, res) => {
   const { prompt } = req.body;
